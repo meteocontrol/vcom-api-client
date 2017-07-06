4201643dd91f707618b36d2bb3600caa0cc02b95
Albert Wang <a.wang@meteocontrol.com>
2017-07-06 11:44:17 +0800

    Add AuthorizationHandler tests

--- tests/unit/lib/ApiClientTest.php
+++ tests/unit/lib/ApiClientTest.php
@@ -3,7 +3,10 @@
 namespace meteocontrol\client\vcomapi;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Request;
 use meteocontrol\client\vcomapi\handlers\BasicAuthorizationHandler;
+use meteocontrol\client\vcomapi\handlers\OAuthAuthorizationHandler;
 
 function usleep($us) {
     ApiClientTest::$us = $us;
@@ -171,6 +174,195 @@ class ApiClientTest extends \PHPUnit_Framework_TestCase {
         $apiClient->run('url', ['name' => 'aa', 'value' => 'bb'], 'patch body', 'UNKNOWN');
     }
 
+    /**
+     * @expectedException \meteocontrol\client\vcomapi\UnauthorizedException
+     * @expectedExceptionMessage Unauthorized. Please check your username and password!
+     */
+    public function testRunWithBasicAuthenticationUnauthorized() {
+        $config = new Config();
+
+        $request = new Request('GET', 'url');
+        $responseMock = $this->getMockBuilder('GuzzleHttp\Psr7\Response')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $responseMock->expects($this->exactly(2))
+            ->method('getStatusCode')
+            ->willReturn(401);
+        $clientException = new ClientException('', $request, $responseMock);
+
+        /** @var Client|\PHPUnit_Framework_MockObject_MockObject $client */
+        $client = $this->getMockBuilder('\GuzzleHttp\Client')
+            ->setMethods(['get'])
+            ->getMock();
+        $client->expects($this->once())
+            ->method('get')
+            ->with('url')
+            ->willThrowException($clientException);
+
+        $authHandler = new BasicAuthorizationHandler($config);
+        $apiClient = new ApiClient($client, $authHandler);
+        $apiClient->run('url');
+    }
+
+    public function testRunWithOAuthUnauthorizedAndRefreshTokenIsValid() {
+        $config = new Config(__DIR__ . '/_files/config.ini');
+
+        $responseMockPasswordGrant = $this->getResponseMockForOAuthPasswordGrant();
+
+        $responseMockRefreshGrant = $this->getResponseMockForOAuthRefreshGrant();
+
+        $streamMock = $this->getMockBuilder('GuzzleHttp\Psr7\BufferStream')
+            ->disableOriginalConstructor()
+            ->setMethods(['getContents'])
+            ->getMock();
+        $streamMock->expects($this->once())
+            ->method('getContents')
+            ->willReturn('123');
+        $responseMock = $this->getMockBuilder('Guzzle\Http\Message\Response')
+            ->disableOriginalConstructor()
+            ->setMethods(['getHeaderLine', 'getBody'])
+            ->getMock();
+        $responseMock->expects($this->once())
+            ->method('getBody')
+            ->willReturn($streamMock);
+        $responseMock->expects($this->exactly(1))
+            ->method('getHeaderLine')
+            ->withConsecutive(['X-RateLimit-Remaining-Minute'])
+            ->willReturn('10');
+
+        /** @var Client|\PHPUnit_Framework_MockObject_MockObject $client */
+        $client = $this->getMockBuilder('\GuzzleHttp\Client')
+            ->setMethods(['get', 'post'])
+            ->getMock();
+        $client->expects($this->exactly(2))
+            ->method('get')
+            ->withConsecutive(['url'], ['url'])
+            ->willReturnOnConsecutiveCalls(
+                $this->returnCallback(function () {
+                    $request = new Request('GET', 'url');
+                    $responseMock = $this->getMockBuilder('GuzzleHttp\Psr7\Response')
+                        ->disableOriginalConstructor()
+                        ->getMock();
+                    $responseMock->expects($this->exactly(2))
+                        ->method('getStatusCode')
+                        ->willReturn(401);
+                    throw new ClientException('', $request, $responseMock);
+                }),
+                $responseMock
+            );
+        $client->expects($this->exactly(2))
+            ->method('post')
+            ->withConsecutive([
+                'https://test.meteocontrol.api/login',
+                [
+                    'form_params' => [
+                        'grant_type' => 'password',
+                        'username' => 'test-api-username',
+                        'password' => 'test-api-password',
+                        'client_id' => 'mc-php-client',
+                        'client_secret' => 'jtL1FR9xg]NX4Mq<mzre}F-TypZ_k,ib'
+                    ]
+                ]
+            ], [
+                'https://test.meteocontrol.api/login',
+                [
+                    'form_params' => [
+                        'grant_type' => 'refresh_token',
+                        'refresh_token' => 'refreshToken',
+                        'client_id' => 'mc-php-client',
+                        'client_secret' => 'jtL1FR9xg]NX4Mq<mzre}F-TypZ_k,ib'
+                    ]
+                ]
+            ])
+            ->willReturnOnConsecutiveCalls(
+                $responseMockPasswordGrant,
+                $responseMockRefreshGrant
+            );
+
+        $authHandler = new OAuthAuthorizationHandler($config);
+        $apiClient = new ApiClient($client, $authHandler);
+        $this->assertEquals('123', $apiClient->run('url'));
+    }
+
+    /**
+     * @expectedException \meteocontrol\client\vcomapi\UnauthorizedException
+     * @expectedExceptionMessage {"hint":"refresh token is revoked"}
+     */
+    public function testRunWithOAuthUnauthorizedAndRefreshTokenIsInvalid() {
+        $config = new Config(__DIR__ . '/_files/config.ini');
+
+        $responseMockPasswordGrant = $this->getResponseMockForOAuthPasswordGrant();
+
+        $request = new Request('GET', 'url');
+        $responseMock = $this->getMockBuilder('GuzzleHttp\Psr7\Response')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $responseMock->expects($this->exactly(2))
+            ->method('getStatusCode')
+            ->willReturn(401);
+        $clientException = new ClientException('', $request, $responseMock);
+
+        /** @var Client|\PHPUnit_Framework_MockObject_MockObject $client */
+        $client = $this->getMockBuilder('\GuzzleHttp\Client')
+            ->setMethods(['get', 'post'])
+            ->getMock();
+        $client->expects($this->once())
+            ->method('get')
+            ->with('url')
+            ->willThrowException($clientException);
+        $client->expects($this->exactly(2))
+            ->method('post')
+            ->withConsecutive([
+                'https://test.meteocontrol.api/login',
+                [
+                    'form_params' => [
+                        'grant_type' => 'password',
+                        'username' => 'test-api-username',
+                        'password' => 'test-api-password',
+                        'client_id' => 'mc-php-client',
+                        'client_secret' => 'jtL1FR9xg]NX4Mq<mzre}F-TypZ_k,ib'
+                    ]
+                ]
+            ], [
+                'https://test.meteocontrol.api/login',
+                [
+                    'form_params' => [
+                        'grant_type' => 'refresh_token',
+                        'refresh_token' => 'refreshToken',
+                        'client_id' => 'mc-php-client',
+                        'client_secret' => 'jtL1FR9xg]NX4Mq<mzre}F-TypZ_k,ib'
+                    ]
+                ]
+            ])
+            ->willReturnOnConsecutiveCalls(
+                $responseMockPasswordGrant,
+                $this->returnCallback(function () {
+                    $streamMock = $this->getMockBuilder('GuzzleHttp\Psr7\BufferStream')
+                        ->disableOriginalConstructor()
+                        ->setMethods(['getContents'])
+                        ->getMock();
+                    $streamMock->expects($this->once())
+                        ->method('getContents')
+                        ->willReturn(json_encode(['hint' => 'refresh token is revoked']));
+                    $responseMock = $this->getMockBuilder('GuzzleHttp\Psr7\Response')
+                        ->disableOriginalConstructor()
+                        ->getMock();
+                    $responseMock->expects($this->exactly(2))
+                        ->method('getStatusCode')
+                        ->willReturn(401);
+                    $responseMock->expects($this->once())
+                        ->method('getBody')
+                        ->willReturn($streamMock);
+                    $request = new Request('POST', '/login');
+                    throw new ClientException('refresh token is revoked', $request, $responseMock);
+                })
+            );
+
+        $authHandler = new OAuthAuthorizationHandler($config);
+        $apiClient = new ApiClient($client, $authHandler);
+        $apiClient->run('url');
+    }
+
     public function testRateLimitHandling() {
         $streamMock = $this->getMockBuilder('GuzzleHttp\Psr7\BufferStream')
             ->disableOriginalConstructor()
@@ -229,5 +421,47 @@ class ApiClientTest extends \PHPUnit_Framework_TestCase {
             ->with('X-RateLimit-Remaining-Minute')
             ->willReturn('100');
         return $responseMock;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getResponseMockForOAuthPasswordGrant() {
+        $streamMockPasswordGrant = $this->getMockBuilder('GuzzleHttp\Psr7\BufferStream')
+            ->disableOriginalConstructor()
+            ->setMethods(['getContents'])
+            ->getMock();
+        $streamMockPasswordGrant->expects($this->once())
+            ->method('getContents')
+            ->willReturn(json_encode(['access_token' => 'accessToken', 'refresh_token' => 'refreshToken']));
+        $responseMockPasswordGrant = $this->getMockBuilder('Guzzle\Http\Message\Response')
+            ->disableOriginalConstructor()
+            ->setMethods(['getHeaderLine', 'getBody'])
+            ->getMock();
+        $responseMockPasswordGrant->expects($this->once())
+            ->method('getBody')
+            ->willReturn($streamMockPasswordGrant);
+        return $responseMockPasswordGrant;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getResponseMockForOAuthRefreshGrant() {
+        $streamMockRefreshGrant = $this->getMockBuilder('GuzzleHttp\Psr7\BufferStream')
+            ->disableOriginalConstructor()
+            ->setMethods(['getContents'])
+            ->getMock();
+        $streamMockRefreshGrant->expects($this->once())
+            ->method('getContents')
+            ->willReturn(json_encode(['access_token' => 'accessToken1', 'refresh_token' => 'refreshToken1']));
+        $responseMockRefreshGrant = $this->getMockBuilder('Guzzle\Http\Message\Response')
+            ->disableOriginalConstructor()
+            ->setMethods(['getHeaderLine', 'getBody'])
+            ->getMock();
+        $responseMockRefreshGrant->expects($this->once())
+            ->method('getBody')
+            ->willReturn($streamMockRefreshGrant);
+        return $responseMockRefreshGrant;
     }
 }
