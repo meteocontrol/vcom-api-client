e99f3bf3f0ed12500f06238386fcc3b7365e1e3c
Albert Wang <a.wang@meteocontrol.com>
2017-07-07 16:30:26 +0800

    Populate server 401 error message to UnauthorizedException

--- src/handlers/AuthorizationHandlerInterface.php
+++ src/handlers/AuthorizationHandlerInterface.php
@@ -3,14 +3,16 @@
 namespace meteocontrol\client\vcomapi\handlers;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use meteocontrol\client\vcomapi\UnauthorizedException;
 
 interface AuthorizationHandlerInterface {
     /**
+     * @param ClientException $ex
      * @param Client $client
      * @throws UnauthorizedException
      */
-    public function handleUnauthorizedException(Client $client);
+    public function handleUnauthorizedException(ClientException $ex, Client $client);
     /**
      * @param Client $client
      * @param array $options
