3c617132ec941ae78ce380fca5b6b14c9e1c34f9
Albert Wang <a.wang@meteocontrol.com>
2017-07-19 14:01:55 +0800

    Add battery endpoints

---
+++ src/model/Battery.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace meteocontrol\client\vcomapi\model;
+
+class Battery extends BaseModel {
+
+    /** @var string */
+    public $id;
+
+    /** @var string */
+    public $name;
+}
