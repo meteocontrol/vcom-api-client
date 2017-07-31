470bfd35803c504b988a3c03a7cdfb74c0f1e6b1
Hammond Lai <h.lai@meteocontrol.com>
2017-07-31 16:19:08 +0800

    add stringbox device endpoints

--- src/endpoints/sub/systems/Stringbox.php
+++ src/endpoints/sub/systems/Stringbox.php
@@ -4,32 +4,25 @@ namespace meteocontrol\client\vcomapi\endpoints\sub\systems;
 
 use meteocontrol\client\vcomapi\endpoints\EndpointInterface;
 use meteocontrol\client\vcomapi\endpoints\sub\SubEndpoint;
-use meteocontrol\client\vcomapi\model\Stringbox;
+use meteocontrol\client\vcomapi\model\StringboxDetail;
 
-class StringBoxes extends SubEndpoint {
+class Stringbox extends SubEndpoint {
 
     /**
      * @param EndpointInterface $parent
      */
     public function __construct(EndpointInterface $parent) {
-        $this->uri = '/stringboxes';
+        $this->uri = '/stringbox';
         $this->api = $parent->getApiClient();
         $this->parent = $parent;
     }
 
     /**
-     * Stringbox[]
+     * @return StringboxDetail
      */
     public function get() {
         $invertersJson = $this->api->run($this->getUri());
         $decodedJson = json_decode($invertersJson, true);
-        return Stringbox::deserializeArray($decodedJson['data']);
-    }
-
-    /**
-     * @return Bulk
-     */
-    public function bulk() {
-        return new Bulk($this);
+        return StringboxDetail::deserialize($decodedJson['data']);
     }
 }
