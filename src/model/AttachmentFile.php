b6b210599cf60a85e400df920c5d4f6c6b030c60
Hammond Lai <h.lai@meteocontrol.com>
2017-08-31 15:47:15 +0800

    refactoring: remove setter/getter in AttachmentFile and return AttachmentFile[] in GetAttachments endpoint

--- src/model/AttachmentFile.php
+++ src/model/AttachmentFile.php
@@ -5,36 +5,22 @@ namespace meteocontrol\client\vcomapi\model;
 class AttachmentFile extends BaseModel {
 
     /** @var int */
-    private $attachmentId;
+    public $attachmentId;
 
     /** @var string */
-    private $filename;
+    public $filename;
 
     /** @var string */
-    private $content;
+    public $content;
 
     /** @var string */
-    private $encodedContent;
-
-    /** @var string */
-    private $description;
+    public $description;
 
     /** @var int */
-    private $creatorId;
+    public $creatorId;
 
     /** @var \DateTime */
-    private $created;
-
-    /**
-     * @param string | null $filename
-     * @param string | null $content
-     * @param string | null $description
-     */
-    public function __construct($filename = null, $content = null, $description = null) {
-        $this->setContent($content);
-        $this->setFilename($filename);
-        $this->setDescription($description);
-    }
+    public $created;
 
     /**
      * @param array $data
@@ -46,148 +32,23 @@ class AttachmentFile extends BaseModel {
         $classInstance = new $className();
         foreach ($data as $key => $value) {
             if (property_exists($className, $key)) {
-                $classInstance->{self::getSetterMethodName($key)}(self::getPhpValue($value));
+                if ($key == "content") {
+                    $classInstance->{$key} = self::getEncodedContent($value);
+                } else {
+                    $classInstance->{$key} = self::getPhpValue($value);
+                }
             }
         }
         return $classInstance;
     }
 
     /**
-     * @param int $id
-     */
-    public function setId($id) {
-        $this->attachmentId = $id;
-    }
-
-    /**
-     * @return int
-     */
-    public function getId() {
-        return $this->attachmentId;
-    }
-
-    /**
-     * @param string $content
-     */
-    public function setContent($content) {
-        $this->content = $content;
-        $this->encodedContent = $this->base64Encode($content);
-    }
-
-    /**
-     * @return string
-     */
-    public function getContent() {
-        return $this->content;
-    }
-
-    /**
      * @param string $encodedContent
+     * @return bool|string
      */
-    public function setEncodedContent($encodedContent) {
-        $this->content = $this->base64Decode($encodedContent);
-        $this->encodedContent = $encodedContent;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEncodedContent() {
-        return $this->encodedContent;
-    }
-
-    /**
-     * @param string $filename
-     */
-    public function setFilename($filename) {
-        $this->filename = $filename ? basename($filename) : null;
-    }
-
-    /**
-     * @return string
-     */
-    public function getFilename() {
-        return $this->filename;
-    }
-
-    /**
-     * @param string $description
-     */
-    public function setDescription($description) {
-        $this->description = $description;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDescription() {
-        return $this->description;
-    }
-
-    /**
-     * @param int $creatorId
-     */
-    public function setCreatorId($creatorId) {
-        $this->creatorId = $creatorId;
-    }
-
-    /**
-     * @return int
-     */
-    public function getCreatorId() {
-        return $this->creatorId;
-    }
-
-    /**
-     * @return \DateTime
-     */
-    public function getCreated() {
-        return $this->created;
-    }
-
-    /**
-     * @param \DateTime $created
-     */
-    public function setCreated(\DateTime $created) {
-        $this->created = $created;
-    }
-
-    /**
-     * @param string $content
-     * @param string $type
-     * @return string | null
-     */
-    private function base64Encode($content, $type = "image/jpeg") {
-        if (!$content) {
-            return null;
-        }
-        return 'data:' . $type . ';base64,' . base64_encode($content);
-    }
-
-    /**
-     * @param string $encodedContent
-     * @return string
-     */
-    private function base64Decode($encodedContent) {
+    private static function getEncodedContent($encodedContent) {
         list(, $data) = explode(';', $encodedContent);
         list(, $data) = explode(',', $data);
         return base64_decode($data);
-    }
-
-    /**
-     * @param string $key
-     * @return string
-     */
-    private static function getSetterMethodName($key) {
-        $param = $key;
-        switch ($key) {
-            case "content":
-                $param = "encodedContent";
-                break;
-            case "attachmentId":
-                $param = "id";
-                break;
-        }
-        return "set" . ucfirst($param);
     }
 }
