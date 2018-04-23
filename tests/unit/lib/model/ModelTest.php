b41329451c92344e7e477755816d26fdcacf1279
Deray Hsueh <d.hsueh@meteocontrol.com>
2018-04-23 16:33:49 +0800

    Add more variable type to test case, concat code

--- tests/unit/lib/model/ModelTest.php
+++ tests/unit/lib/model/ModelTest.php
@@ -5,6 +5,7 @@ namespace meteocontrol\client\vcomapi;
 use meteocontrol\vcomapi\model\AttachmentFile;
 use meteocontrol\vcomapi\model\Comment;
 use meteocontrol\vcomapi\model\CommentDetail;
+use meteocontrol\vcomapi\model\Coordinates;
 use meteocontrol\vcomapi\model\MeasurementValue;
 use meteocontrol\vcomapi\model\SystemDetail;
 use meteocontrol\vcomapi\model\Ticket;
@@ -14,37 +15,54 @@ class ModelTest extends \PHPUnit_Framework_TestCase {
 
     public function testModelEncodeToJson() {
         $dateTime = new \DateTime('2018-01-02T03:04:01+00:00');
+        $intValue = 1;
+        $floatValue = 0.1;
+        $stringValue = 'string';
+        $booleanValue = true;
         $expectedData = json_decode(file_get_contents(__DIR__ . '/_files/models.json'), true);
 
         $attachmentFile = new AttachmentFile();
-        $attachmentFile->created =
-        $attachmentFile->createdAt = $dateTime;
+        $attachmentFile->attachmentId = $attachmentFile->creatorId = $intValue;
+        $attachmentFile->filename = $attachmentFile->content = $attachmentFile->description = $stringValue;
+        $attachmentFile->created = $attachmentFile->createdAt = $dateTime;
 
         $comment = new Comment();
-        $comment->date =
-        $comment->createdAt = $dateTime;
+        $comment->commentId = $intValue;
+        $comment->comment = $comment->username = $comment->firstName =
+        $comment->lastName = $stringValue;
+        $comment->date = $comment->createdAt = $dateTime;
 
         $commentDetail = new CommentDetail();
-        $commentDetail->date =
-        $commentDetail->createdAt = $dateTime;
+        $commentDetail->commentId = $intValue;
+        $commentDetail->comment = $commentDetail->username = $commentDetail->firstName =
+        $commentDetail->lastName = $stringValue;
+        $commentDetail->date = $commentDetail->createdAt = $dateTime;
 
         $measurementValue = new MeasurementValue();
+        $measurementValue->value = $stringValue;
         $measurementValue->timestamp = $dateTime;
 
         $ticket = new Ticket();
-        $ticket->date =
-        $ticket->lastChange =
-        $ticket->rectifiedOn =
-        $ticket->createdAt =
-        $ticket->lastChangedAt =
-        $ticket->rectifiedAt = $dateTime;
+        $ticket->id = $ticket->causeId = $intValue;
+        $ticket->systemKey = $ticket->designation = $ticket->summary = $ticket->assignee =
+        $ticket->status = $ticket->priority = $ticket->includeInReports = $ticket->severity =
+        $ticket->description = $stringValue;
+        $ticket->fieldService = $booleanValue;
+        $ticket->date = $ticket->lastChange = $ticket->rectifiedOn = $ticket->createdAt =
+        $ticket->lastChangedAt = $ticket->rectifiedAt = $dateTime;
 
         $ticketHistory = new TicketHistory();
-        $ticketHistory->timestamp =
-        $ticketHistory->createdAt = $dateTime;
+        $ticketHistory->action = $ticketHistory->personInCharge = $ticketHistory->from =
+        $ticketHistory->to = $stringValue;
+        $ticketHistory->timestamp = $ticketHistory->createdAt = $dateTime;
 
         $systemDetail = new SystemDetail();
+        $systemDetail->elevation = $intValue;
+        $systemDetail->name = $systemDetail->currency = $stringValue;
         $systemDetail->commissionDate = $dateTime;
+
+        $coordinates = new Coordinates();
+        $coordinates->latitude = $coordinates->longitude = $floatValue;
 
         $this->assertEquals($expectedData['attachmentFile'], json_decode(json_encode($attachmentFile), true));
         $this->assertEquals($expectedData['comment'], json_decode(json_encode($comment), true));
@@ -53,5 +71,6 @@ class ModelTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals($expectedData['ticket'], json_decode(json_encode($ticket), true));
         $this->assertEquals($expectedData['ticketHistory'], json_decode(json_encode($ticketHistory), true));
         $this->assertEquals($expectedData['systemDetail'], json_decode(json_encode($systemDetail), true));
+        $this->assertEquals($expectedData['coordinates'], json_decode(json_encode($coordinates), true));
     }
 }
