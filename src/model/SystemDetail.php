0829767944a433b289eeedd77186872cd03b89ef
Deray Hsueh <d.hsueh@meteocontrol.com>
2018-05-18 11:22:52 +0800

    Add deserializeCommissionDate

--- src/model/SystemDetail.php
+++ src/model/SystemDetail.php
@@ -34,6 +34,8 @@ class SystemDetail extends BaseModel {
                 $object->coordinates = Coordinates::deserialize($value);
             } elseif (is_array($value) && $key === "timezone") {
                 $object->timezone = Timezone::deserialize($value);
+            } elseif ($key === "commissionDate") {
+                $object->commissionDate = self::deserializeCommissionDate($value, $data);
             } elseif (property_exists($object, $key)) {
                 $object->{$key} = self::getPhpValue($value);
             }
@@ -51,5 +53,18 @@ class SystemDetail extends BaseModel {
             return $dateTime->format('Y-m-d');
         }
         return parent::serializeDateTime($dateTime);
+    }
+
+    /**
+     * @param string $dateString
+     * @param array $data
+     * @return bool|\DateTime
+     */
+    private static function deserializeCommissionDate($dateString, array $data) {
+        return \DateTime::createFromFormat(
+            'Y-m-d H:i:s',
+            "{$dateString} 00:00:00",
+            isset($data['timezone']['name']) ? new \DateTimeZone($data['timezone']['name']) : null
+        );
     }
 }
