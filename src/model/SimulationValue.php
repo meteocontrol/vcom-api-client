3cd290f64adaf7b9f54900218298158de06e3dee
Albert Wang <a.wang@meteocontrol.com>
2021-02-09 16:42:15 +0800

    Release simulation endpoint

---
+++ src/model/SimulationValue.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace meteocontrol\vcomapi\model;
+
+use DateTime;
+
+class SimulationValue extends BaseModel {
+
+    /** @var DateTime */
+    public $timestamp;
+
+    /** @var float */
+    public $max;
+
+    /** @var float */
+    public $min;
+
+    /** @var float */
+    public $expected;
+}
